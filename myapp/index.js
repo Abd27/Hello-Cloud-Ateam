2224abf699ebc7084d41e11036993370e8421ced
Abdur Rehman <wabdulrehman@yahoo.com>
2022-01-07 09:32:47 +0000

    Landing page display the link to miro board

--- myapp/index.js
+++ myapp/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello Makers, we are the A TEAM')
+  //res.send('Hello Makers, we are the A TEAM')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(port, () => {
